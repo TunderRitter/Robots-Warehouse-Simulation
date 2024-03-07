2730c524b174a1cde14da363596a62cbba3759f1
Mészáros Gergő <j56jl1@inf.elte.hu>
2024-03-07 15:12:30 +0100

    Implemented ReadCoordinates #6

--- Warehouse_Simulation_Model/Persistence/ConfigReader.cs
+++ Warehouse_Simulation_Model/Persistence/ConfigReader.cs
@@ -5,46 +5,53 @@ namespace Warehouse_Simulation_Model.Persistence;
 
 public static class ConfigReader
 {
-    public static void Read(string path)
+    public static SchedulerData Read(string path)
     {
-		try
-		{
-			ConfigFile config = JsonSerializer.Deserialize<ConfigFile>(File.ReadAllText(path));
-            ReadMap(config.mapFile);
-            ReadRobots(config.agentFile);
-            ReadTargets(config.taskFile);
-		}
-		catch (Exception)
-		{
-			throw;
-		}
-	}
+        try
+        {
+            ConfigFile config = JsonSerializer.Deserialize<ConfigFile>(File.ReadAllText(path));
+            bool[,] map = ReadMap(config.mapFile);
+            (int, int)[] robots = ReadCoordinates(config.agentFile, map);
+            (int, int)[] targets = ReadCoordinates(config.taskFile, map);
+            return new SchedulerData
+            {
+                Robots = robots,
+                Targets = targets,
+                TasksSeen = config.numTasksReveal,
+                Map = map,
+            };
+        }
+        catch (Exception)
+        {
+            throw;
+        }
+    }
 
     public static bool[,] ReadMap(string path)
     {
-		try
-		{
+        try
+        {
             string FullContent = File.ReadAllText(path);
             string[] Lines = FullContent.Split("\n");
             int Height = int.Parse(Lines[1].Split(" ")[1]);
             int Width = int.Parse(Lines[2].Split(" ")[1]);
-			bool[,] Map = new bool[Height, Width];
+            bool[,] Map = new bool[Height, Width];
             if (Map.GetLength(1) == 0 || Map.GetLength(0) == 0) throw new ArgumentOutOfRangeException("Map must be at least 1x1");
             if (Lines.Length - 4 != Height)
-			{
-				throw new ArgumentException("The number of lines in the map doesn't match the height.");
-			}
-			for (int i = 4; i < Lines.Length; i++)
-			{
-				if (Lines[i].Length != Width)
-				{
-					throw new ArgumentException("The length of a line doesn't match the map's width.");
-				}
-				for(int j = 0; j < Width; j++)
-				{
-					Map[i - 4, j] = !(Lines[i][j] == '.');
-				}
-			}
+            {
+                throw new ArgumentException("The number of lines in the map doesn't match the height.");
+            }
+            for (int i = 4; i < Lines.Length; i++)
+            {
+                if (Lines[i].Length != Width)
+                {
+                    throw new ArgumentException("The length of a line doesn't match the map's width.");
+                }
+                for(int j = 0; j < Width; j++)
+                {
+                    Map[i - 4, j] = !(Lines[i][j] == '.');
+                }
+            }
             
 
             bool allwall = true;
@@ -61,22 +68,36 @@ public static class ConfigReader
 
             return Map;
         }
-		catch (Exception)
-		{
-			throw;
-		}
-		
+        catch (Exception)
+        {
+            throw;
+        }
+        
     }
 
-	private static void ReadRobots(string path)
-	{
-		List<(int, int)> robots = [];
-	}
+    private static (int, int)[] ReadCoordinates(string path, bool[,] map)
+    {
+        try
+        {
+            int[] lines = Array.ConvertAll(File.ReadAllText(path).Split("\n", StringSplitOptions.RemoveEmptyEntries), int.Parse);
+            if (lines[0] != lines.Length - 1) throw new ArgumentException("Number of coordinates does not match");
+            (int row, int col)[] coors = new (int, int)[lines[0]];
+            for (int i = 1; i < coors.Length; i++)
+            {
+                coors[i] = ConvertCoordinates(lines[i], map.GetLength(1));
+                if (coors[i].row < 0
+                    || coors[i].row >= map.GetLength(0)
+                    || coors[i].col < 0
+                    || coors[i].col >= map.GetLength(1)) throw new ArgumentException("Out of bounds");
+                if (map[coors[i].row, coors[i].col]) throw new ArgumentException("Wall");
+            }
+            return coors;
+        }
+        catch (Exception)
+        {
+            throw;
+        }
+    }
 
-	private static void ReadTargets(string path)
-	{
-
-	}
-
-	private static (int, int) ConvertCoordinates(int coor, int width) => (coor / width, coor % width);
+    private static (int, int) ConvertCoordinates(int coor, int width) => (coor / width, coor % width);
 }
--- Warehouse_Simulation_Model/Persistence/SchedulerData.cs
+++ Warehouse_Simulation_Model/Persistence/SchedulerData.cs
@@ -1,69 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Cryptography;
-using System.Text;
-using System.Threading.Tasks;
-using Warehouse_Simulation_Model.Model;
+﻿namespace Warehouse_Simulation_Model.Persistence;
 
-namespace Warehouse_Simulation_Model.Persistence
+public readonly struct SchedulerData
 {
-    public class SchedulerData
-    {
-        public (int, int)[]? _robots { get; private set; }
-        public (int, int)[]? _targets { get; private set; }
-        public int tasksSeen { get; private set; }
-        public bool[,]? _map { get; private set; }
-
-        public SchedulerData((int, int)[] robots, (int, int)[] targets, int tasksSeen, bool[,] map)
-        {
-            if (map == null) throw new ArgumentNullException("Map is null");
-            if (robots == null) throw new ArgumentNullException("Robots are null");
-            if (targets == null) throw new ArgumentNullException("Targets are null");
-            if (tasksSeen <= 0) throw new ArgumentOutOfRangeException("Number of seen tasks must be above zero");
-
-            this.tasksSeen = tasksSeen;
-            if(checkMap(map)) _map = map;
-            if (checkRobots(robots, map)) _robots = robots;
-            if (checkTargets(targets, map)) _targets = targets;
-        }
-
-        private bool checkMap(bool[,] map)
-        {
-            if (map.Length == 0 || map.GetLength(0) == 0) throw new ArgumentOutOfRangeException("Map must be at least 1x1");
-
-            bool allwall = true;
-            for (int i = 0; i < map.Length; i++)
-            {
-                for (int j = 0; j < map.GetLength(i); j++)
-                {
-                    if (!map[i, j]) allwall = false;
-                }
-            }
-
-            if (allwall) throw new ArgumentException("Map can't be just walls");
-
-            return true;
-        }
-
-        private bool checkRobots((int x, int y)[] robots, bool[,] map)
-        {
-            for (int i = 0; i < robots.Length; i++)
-            {
-                if (robots[i].x < 0 || robots[i].y < 0 || robots[i].y > map.Length || robots[i].x > map.GetLength(0)) throw new ArgumentOutOfRangeException("Robots' coordinates must be within 0 and the size of the map");
-                if (map[robots[i].x, robots[i].y]) throw new ArgumentException("Robot's coordinates can't be within a wall");
-            }
-            return true;
-        }
-
-        private bool checkTargets((int x, int y)[] targets, bool[,] map)
-        {
-            for (int i = 0; i < targets.Length; i++)
-            {
-                if (targets[i].x < 0 || targets[i].y < 0 || targets[i].y > map.Length || targets[i].x > map.GetLength(0)) throw new ArgumentOutOfRangeException("Targets' coordinates must be within 0 and the size of the map");
-                if (map[targets[i].x, targets[i].y]) throw new ArgumentException("Target's coordinates can't be within a wall");
-            }
-            return true;
-        }
-    }
+    public (int, int)[] Robots { get; init; }
+    public (int, int)[] Targets { get; init; }
+    public int TasksSeen { get; init; }
+    public bool[,] Map { get; init; }
 }
