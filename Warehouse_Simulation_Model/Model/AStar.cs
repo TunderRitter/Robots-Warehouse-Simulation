5f3691e06faea99b670ac48fe1dff7aeec6ba90d
otoanr <otoanr@inf.elte.hu>
2024-03-14 13:02:10 +0100

    Made a class for cells used in AStar #11

---
+++ Warehouse_Simulation_Model/Model/AStar.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warehouse_Simulation_Model.Model;
+
+public class AStarCell
+{
+    public int I, J;
+    public AStarCell? Parent;
+    // g = távolság a kiindulóponttól
+    // h = távolság a célállomástól (légvonalban, falakat nem figyelve)
+    // f = g + h
+    public int f, g, h;
+
+    public AStarCell(int i, int j, AStarCell? parent)
+    {
+        I = i; J = j;
+        Parent = parent;
+        f = 0; g = 0; h = 0;
+    }
+}
+public class AStar
+{
+
+}
