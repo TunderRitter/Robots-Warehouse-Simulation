ff10203bc3e3d40fe58de2c0c8082c081d92a8bc
Mészáros Gergő <j56jl1@inf.elte.hu>
2024-03-21 15:22:37 +0100

    Implemented RoundRobin #28

--- Warehouse_Simulation_Model/Model/RoundRobin.cs
+++ Warehouse_Simulation_Model/Model/RoundRobin.cs
@@ -5,6 +5,12 @@ public class RoundRobin : ITaskAssigner
 {
     public void Assign(Robot[] robots, Target[] targets)
     {
+        if (robots.Length != targets.Length) throw new ArgumentException("Robots and targets are different size");
 
+        for (int i = 0; i < robots.Length; i++)
+        {
+            robots[i].TargetPos = targets[i].Pos;
+            targets[i].Id = robots[i].Id;
+        }
     }
 }
