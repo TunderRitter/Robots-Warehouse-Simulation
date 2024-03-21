0191fcad6ed66e0567970744e591ea907d0d8244
Mészáros Gergő <j56jl1@inf.elte.hu>
2024-03-21 15:13:44 +0100

    Created constructor #26

--- Warehouse_Simulation_Model/Model/Scheduler.cs
+++ Warehouse_Simulation_Model/Model/Scheduler.cs
@@ -8,35 +8,75 @@ public class Scheduler
 {
     private readonly Robot[] _robots;
     private readonly Queue<Target> _targets;
-    private readonly double _timeLimit;
     private readonly Log _log;
     private readonly Queue<(int, int)>[] _routes;
-    private readonly ITaskAssigner _method;
+    private readonly ITaskAssigner _strategy;
     private readonly AStar _astar;
-    private readonly int _to_step;
+    private readonly double _timeLimit;
+    private readonly int _steps;
+    private readonly int _teamSize;
+    private bool _robotFreed;
 
-    private Cell[,] _map;
-    public Cell[,] Map => _map; // Encapsulation!
+    public Cell[,] Map { get; private set; } // Encapsulation!
     public int Steps { get; private set; }
 
 
-    public Scheduler(int mapWidth, int mapHeight, Robot[] robots, double timeLimit, Log log, ITaskAssigner method)
+    public Scheduler(SchedulerData data)
     {
+        int height = data.Map.GetLength(0);
+        int width = data.Map.GetLength(1);
+        Map = new Cell[height, width];
+
+        for (int i = 0; i < height; i++)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                if (data.Map[i, j])
+                    Map[i, j] = new Wall();
+                else
+                    Map[i, j] = new Floor();
+            }
+        }
+        _robots = new Robot[data.Robots.Length];
+        for (int i = 0; i < data.Robots.Length; i++)
+        {
+            Robot robot = new(i, data.Robots[i], Direction.N);
+            _robots[i] = robot;
+            ((Floor)Map[robot.Pos.row, robot.Pos.col]).Robot = robot;
+        }
+        _targets = new Queue<Target>();
+        foreach ((int row, int col) target in data.Targets)
+        {
+            _targets.Enqueue(new Target(target));
+        }
+
+        _log = new Log();
+        _routes = new Queue<(int, int)>[data.Robots.Length];
+        _strategy = TaskAssignerFactory.Create(data.Strategy);
+        _astar = new AStar(data.Map);
+
+        _timeLimit = 10; // !!!
+        _steps = 10; // !!!
+        _teamSize = Math.Min(data.TeamSize, data.Robots.Length);
+        _robotFreed = false;
+
+        AssignTasks();
+
         Schedule();
     }
 
     private void Schedule()
     {
         //System.Timers.Timer timer = new System.Timers.Timer();
-
-        for (int i = 0; i < _robots.Length; i++)
+        if (_robotFreed)
         {
-            AssignTask(_robots[i]);
+            AssignTasks();
+            _robotFreed = false;
         }
 
         CalculateRoutes();
 
-        while(_targets.Count > 0 && Steps >= _to_step) ;
+        while(_targets.Count > 0 && Steps >= _steps) ;
         {
             for (int i = 0; i < _robots.Length; i++)
             {
@@ -48,30 +88,15 @@ public class Scheduler
         //System.Threading.Thread.Sleep(1000);
     }
 
-    private void TurnRobotLeft(Robot robot)
-    {
-        robot.TurnLeft();
-    }
+    private static void TurnRobotLeft(Robot robot) => robot.TurnLeft();
 
-    private void TurnRobotRight(Robot robot)
-    {
-        robot.TurnRight();
-    }
+    private static void TurnRobotRight(Robot robot) => robot.TurnRight();
 
-    private void Robot_Finished(object? sender, int e)
-    {
-        if (sender != null && sender is Robot robot) AssignTask(robot);
-    }
+    private void Robot_Finished(object? sender, int e) => _robotFreed = true;
 
-    public void AssignTask(Robot robot)
+    public void AssignTasks()
     {
-        robot.TargetPos = null;
-        if(_targets.Count > 0)
-        {
-            Target target = _targets.Dequeue();
-            (int X, int Y) pos = target.Pos;
-            robot.TargetPos = (pos.X, pos.Y);
-        }
+        
     }
 
     public void CalculateStep(Robot robot, int i)
