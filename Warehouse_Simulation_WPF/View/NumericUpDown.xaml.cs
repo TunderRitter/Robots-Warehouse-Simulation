89656921a4afb0e790eee526b3464fa8a61afff7
Mészáros Gergő <j56jl1@inf.elte.hu>
2024-03-23 17:35:09 +0100

    Created NumericUpDown User Control #30

---
+++ Warehouse_Simulation_WPF/View/NumericUpDown.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Warehouse_Simulation_WPF.View;
+
+
+public partial class NumericUpDown : UserControl
+{
+    private bool _selected = false;
+
+    private int _min = 0;
+    public int Min
+    {
+        get => _min;
+        set
+        {
+            _min = value;
+            int defValue = Math.Max(value, Default);
+            if (defValue != Default)
+                Default = defValue;
+        }
+    }
+
+    private int _max = 100;
+    public int Max
+    {
+        get => _max;
+        set
+        {
+            _max = value;
+            int defValue = Math.Min(value, Default);
+            if (defValue != Default)
+                Default = defValue;
+        }
+    }
+
+
+    public int Default
+    {
+        get => (int)GetValue(DefaultProperty);
+        set => SetValue(DefaultProperty, value);
+    }
+    public static readonly DependencyProperty DefaultProperty =
+        DependencyProperty.Register("Default", typeof(int), typeof(NumericUpDown), new PropertyMetadata(0));
+
+    public string Value
+    {
+        get => (string)GetValue(ValueProperty);
+        set => SetValue(ValueProperty, value);
+    }
+    public static readonly DependencyProperty ValueProperty =
+        DependencyProperty.Register("Value", typeof(string), typeof(NumericUpDown), new PropertyMetadata("0"));
+
+    public ICommand Command
+    {
+        get => (ICommand)GetValue(CommandProperty);
+        set => SetValue(CommandProperty, value);
+    }
+    public static readonly DependencyProperty CommandProperty =
+        DependencyProperty.Register("Command", typeof(ICommand), typeof(NumericUpDown));
+
+
+
+    public NumericUpDown()
+    {
+        InitializeComponent();
+    }
+}
