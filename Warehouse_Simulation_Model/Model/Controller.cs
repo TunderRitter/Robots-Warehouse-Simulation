e07e08db47d59d4af0e7a72400a96289a527e8c3
Ritter Tünde <e5kqk8@inf.elte.hu>
2024-04-10 16:12:13 +0200

    Added properties to Controller class #36

--- Warehouse_Simulation_Model/Model/Controller.cs
+++ Warehouse_Simulation_Model/Model/Controller.cs
@@ -8,6 +8,10 @@ namespace Warehouse_Simulation_Model.Model
 {
     public class Controller
     {
-
+        private readonly Robot[] _robots;
+        private readonly Queue<(int, int)>[] _routes;
+        private readonly AStar _astar;
+        private const bool _passThrough = false;
+        public Cell[,] Map { get; private set; }
     }
 }
