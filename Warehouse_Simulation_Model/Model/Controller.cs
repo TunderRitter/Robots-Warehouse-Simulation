814cb773ed1d70d231450e2182d70fbd78f2797c
Ritter Tünde <e5kqk8@inf.elte.hu>
2024-04-10 16:29:37 +0200

    Controller class CalculateStep method #36

--- Warehouse_Simulation_Model/Model/Controller.cs
+++ Warehouse_Simulation_Model/Model/Controller.cs
@@ -15,6 +15,74 @@ namespace Warehouse_Simulation_Model.Model
             _astar = astar;
         }
 
+        public (int, String) CalculateStep(Robot robot, Queue<(int, int)> route, bool _passThrough, Cell[,] Map, int i)
+        {
+            (int row, int col) posTo = route.Peek();
+            (int row, int col) posFrom = robot.Pos;
+
+            string move = "";
+
+            if (posFrom.row == posTo.row)
+            {
+                if (posFrom.col - 1 == posTo.col)
+                {
+                    move = robot.Direction switch
+                    {
+                        Direction.N => "C",
+                        Direction.E => "R",
+                        Direction.S => "R",
+                        Direction.W => "F",
+                        _ => throw new Exception(),
+                    };
+
+                    if (!_passThrough && move == "F" && Map[posTo.row, posTo.col] is Floor floor && floor.Robot != null) move = "W";
+                }
+                else if (posFrom.col + 1 == posTo.col)
+                {
+                    move = robot.Direction switch
+                    {
+                        Direction.N => "R",
+                        Direction.E => "F",
+                        Direction.S => "C",
+                        Direction.W => "R",
+                        _ => throw new Exception(),
+                    };
+
+                    if (!_passThrough && move == "F" && Map[posTo.row, posTo.col] is Floor floor && floor.Robot != null) move = "W";
+                }
+            }
+            else if (posFrom.col == posTo.col)
+            {
+                if (posFrom.row - 1 == posTo.row)
+                {
+                    move = robot.Direction switch
+                    {
+                        Direction.N => "F",
+                        Direction.E => "C",
+                        Direction.S => "R",
+                        Direction.W => "R",
+                        _ => throw new Exception(),
+                    };
+
+                    if (!_passThrough && move == "F" && Map[posTo.row, posTo.col] is Floor floor && floor.Robot != null) move = "W";
+                }
+                else if (posFrom.row + 1 == posTo.row)
+                {
+                    move = robot.Direction switch
+                    {
+                        Direction.N => "R",
+                        Direction.E => "R",
+                        Direction.S => "F",
+                        Direction.W => "C",
+                        _ => throw new Exception(),
+                    };
+
+                    if (!_passThrough && move == "F" && Map[posTo.row, posTo.col] is Floor floor && floor.Robot != null) move = "W";
+                }
+            }
+
+            return (i, move);
+        }
 
     }
 }
