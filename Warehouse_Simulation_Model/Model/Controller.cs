3d8f1f20862fa88636418c0ed57b03d456aafede
Ritter Tünde <e5kqk8@inf.elte.hu>
2024-04-10 16:01:14 +0200

    Created Controller class #36

---
+++ Warehouse_Simulation_Model/Model/Controller.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warehouse_Simulation_Model.Model
+{
+    public class Controller
+    {
+
+    }
+}
