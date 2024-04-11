26943c510a95476628134fc087f09b16792c081d
Mészáros Gergő <j56jl1@inf.elte.hu>
2024-04-11 14:59:37 +0200

    Started implementing GetRobots #38

--- Warehouse_Simulation_Model/Model/Replay.cs
+++ Warehouse_Simulation_Model/Model/Replay.cs
@@ -1,60 +1,72 @@
 ﻿using Warehouse_Simulation_Model.Persistence;
 
-namespace Warehouse_Simulation_Model.Model
+namespace Warehouse_Simulation_Model.Model;
+
+
+public class Replay
 {
-    public class Replay
+    private readonly Robot[] _robots;
+    private readonly Target[] _targets;
+    private double _speed;
+    private bool _paused;
+    public Cell[][,] Maps { get; private set; }
+
+
+    public Replay(string logPath)
     {
-        private readonly Robot[] _robots;
-        private readonly Target[] _targets;
-        private double _speed;
-        private bool _paused;
-        private readonly Cell[][,] _map;
-        public Cell[][,] Map => _map;
+		Log log = Log.Read(logPath);
+		_robots = GetRobots(log);
+	}
+
+    public void Read(Log log)
+    {
+
+    }
+
+    public void Play()
+    {
+
+    }
+
+    public void Pause()
+    {
+
+    }
+
+    public void ChangeSpeed(double speed)
+    {
+         
+    }
+
+    public void StepFwd()
+    {
+
+    }
+
+    public void StepBack()
+    {
+
+    }
+
+    public void SkipTo(int step)
+    {
+
+    }
+
+    public void GenerateMaps()
+    {
+
+    }
 
 
-        public Replay()
+    private Robot[] GetRobots(Log log)
+    {
+        if (log.teamSize != log.start.Count)
         {
-
+            // exception
         }
+        Robot[] robots = new Robot[log.teamSize];
 
-        public void Read(Log log)
-        {
-
-        }
-
-        public void Play()
-        {
-
-        }
-
-        public void Pause()
-        {
-
-        }
-
-        public void ChangeSpeed(double speed)
-        {
-             
-        }
-
-        public void StepFwd()
-        {
-
-        }
-
-        public void StepBack()
-        {
-
-        }
-
-        public void SkipTo(int step)
-        {
-
-        }
-
-        public void GenerateMaps()
-        {
-
-        }
+        return robots;
     }
 }
