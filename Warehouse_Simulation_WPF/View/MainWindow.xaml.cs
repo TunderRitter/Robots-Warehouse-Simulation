01e79f10466770b62ed636050cb977882f8e9b76
otoanr <otoanr@inf.elte.hu>
2024-04-19 13:08:02 +0200

    Fixed Back to Menu #32

--- Warehouse_Simulation_WPF/View/MainWindow.xaml.cs
+++ Warehouse_Simulation_WPF/View/MainWindow.xaml.cs
@@ -17,7 +17,7 @@ public partial class MainWindow : Window
         //MenuGrid.Visibility = Visibility.Collapsed;
         //this.WindowState = WindowState.Maximized;
         //SimGrid.Visibility = Visibility.Visible;
-        ReplayStartGrid.Visibility = Visibility.Visible;
+        
     }
     private void SimulationButton_Click(object sender, EventArgs e)
     {
@@ -29,6 +29,7 @@ public partial class MainWindow : Window
     {
         StartGrid.Visibility = Visibility.Collapsed;
         InfoGrid.Visibility = Visibility.Visible;
+        OnlineGrid.Visibility = Visibility.Visible;
     }
     private void HideReplayStartGrid(object sender, EventArgs e)
     {
@@ -36,10 +37,12 @@ public partial class MainWindow : Window
         InfoGrid.Visibility = Visibility.Visible;
         ReplayButtons2.Visibility = Visibility.Visible;
     }
-    private void CloseSim(object sender, EventArgs e)
+    private void Back(object sender, EventArgs e)
     {
         SimGrid.Visibility = Visibility.Collapsed;
-        StartGrid.Visibility = Visibility.Visible;
+        OnlineGrid.Visibility = Visibility.Collapsed;
+        ReplayButtons2.Visibility = Visibility.Collapsed;
+        InfoGrid.Visibility = Visibility.Collapsed;
         MenuGrid.Visibility = Visibility.Visible;
         this.WindowState = WindowState.Normal;
     }
