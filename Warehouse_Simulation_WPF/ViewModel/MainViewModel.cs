877e653102c2e0fc6eaa34f58871ffd2bc4c1dd1
otoanr <otoanr@inf.elte.hu>
2024-04-22 22:16:41 +0200

    fix in replay + disabled window resizing

--- Warehouse_Simulation_WPF/ViewModel/MainViewModel.cs
+++ Warehouse_Simulation_WPF/ViewModel/MainViewModel.cs
@@ -492,6 +492,8 @@ public class MainViewModel : INotifyPropertyChanged
     private void CreateReplayMap()
     {
         if (_replayer == null) return;
+        RobotNumber = "0";
+        TargetLeft = "0";
         StepCount = 0;
         CreateMap(_replayer.InitMap);
     }
@@ -609,6 +611,8 @@ public class MainViewModel : INotifyPropertyChanged
     {
         if (_replayer == null || map == null) return;
 
+        RobotNumber = _replayer.RobotNum.ToString();
+        TargetLeft = _replayer.TargetNum.ToString();
         for (int i = 0; i < Cells.Count; i++)
         {
             int idx = i;
