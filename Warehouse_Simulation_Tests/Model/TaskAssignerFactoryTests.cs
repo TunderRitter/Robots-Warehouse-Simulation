fc6a4289c86f09aa793965dec6eea73b2b7be93f
Ritter Tünde <e5kqk8@inf.elte.hu>
2024-05-13 21:53:01 +0200

    Added tests for TaskAssignerFactory

---
+++ Warehouse_Simulation_Tests/Model/TaskAssignerFactoryTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Warehouse_Simulation_Model.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warehouse_Simulation_Model.Model.Tests
+{
+    [TestClass()]
+    public class TaskAssignerFactoryTests
+    {
+        [TestMethod()]
+        public void CreateTest()
+        {
+            Assert.ThrowsException<InvalidDataException>(() => TaskAssignerFactory.Create("not supported"));
+        }
+    }
+}
