0feb806e58ae42df2aa048f8851d78ab6ad29b3d
Ritter Tünde <e5kqk8@inf.elte.hu>
2024-05-13 21:50:30 +0200

    Added tests for scheduler

--- Warehouse_Simulation_Tests/Model/ControllerTests.cs
+++ Warehouse_Simulation_Tests/Model/ControllerTests.cs
@@ -11,17 +11,27 @@ namespace Warehouse_Simulation_Model.Model.Tests
     [TestClass()]
     public class ControllerTests
     {
-
-        [TestMethod()]
-        public void CalculateRoutesTest()
+        private bool[,] map = new bool[5, 5];
+        private Robot[] robots = new Robot[2];
+        [TestInitialize]
+        public void Setup()
         {
-
+            robots[0] = new Robot(1, (1, 1), Direction.N);
+            robots[1] = new Robot(2, (2, 2), Direction.E);
         }
 
-        [TestMethod()]
-        public void CalculateStepsTest()
+        [TestMethod]
+        public void ControllerTest()
         {
+            if (map == null || robots == null) Assert.Fail();
 
+            Controller controller = new Controller(map, robots);
+            Assert.AreEqual(0, controller.Step);
+
+            controller.CalculateRoutes();
+            controller.CalculateSteps();
+
+            Assert.AreEqual(0, controller.Step);
         }
     }
 }
