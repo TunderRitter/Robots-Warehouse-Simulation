1eb344c35c26a5b0f6b4920b95fc2c5fd2427af2
Ritter Tünde <e5kqk8@inf.elte.hu>
2024-05-13 21:51:43 +0200

    Added tests for Roundrobin

---
+++ Warehouse_Simulation_Tests/Model/RoundRobinTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Warehouse_Simulation_Model.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warehouse_Simulation_Model.Model.Tests
+{
+    [TestClass()]
+    public class RoundRobinTests
+    {
+        [TestMethod()]
+        public void AssignTest()
+        {
+            RoundRobin roundRobin = new RoundRobin();
+            List<Robot> robots = new List<Robot>();
+            List<Target> targets = new List<Target>();
+            robots.Add(new Robot(1, (1, 1), Direction.N));
+            robots.Add(new Robot(2, (2, 2), Direction.E));
+            targets.Add(new Target((1, 3), 1));
+            targets.Add(new Target((3, 3), 2));
+
+            (int, int)[] result = roundRobin.Assign(robots, targets);
+
+            Assert.AreEqual(2, result.Length);
+        }
+    }
+}
