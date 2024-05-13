51a26b3132a7dd731ca67e86942f9ba0faff7f28
Ritter Tünde <e5kqk8@inf.elte.hu>
2024-05-13 21:51:10 +0200

    Added tests for replay

---
+++ Warehouse_Simulation_Tests/Model/ReplayTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Warehouse_Simulation_Model.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warehouse_Simulation_Model.Model.Tests
+{
+    [TestClass()]
+    public class ReplayTests
+    {
+        [TestMethod()]
+        public void ReplayTest()
+        {
+            Replay replay;
+            Assert.ThrowsException<FormatException>(() => replay = new Replay("asd", "asd"));
+        }
+    }
+}
